e83a439e546ae235caa5c0735e8759852643d2be
David Goldberg <drgold1988@gmail.com>
2015-09-12 15:37:01 -0700

    [feat]: adds connection to documentStore db in server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var config = require('./config');
 var GIS = require('./db/postGIS/db');
 var app = express();
-var mongoose = require('mongoose');
 var docStore = require('./db/documentStore/db');
 
 var port = config.port;
