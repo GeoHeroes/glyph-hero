c8d4164763c2de5fddc876b257a581d705da0eb8
David Goldberg <drgold1988@gmail.com>
2015-09-12 18:27:25 -0700

    [test]: creates test coverage for fetching data inputs array of ids, returns doc data

--- db/documentStore/specs/documentStoreQueries.js
+++ db/documentStore/specs/documentStoreQueries.js
@@ -25,8 +25,18 @@ describe('documentStore queries()', function() {
 
   it('should fetch data from the document store', function(done) {
     var testFetchData = Promise.coroutine(function*() {
-      var fetchResult = yield fetchQuery(testData)
-        fetchResult[0].name.should.equal('test');
+      var testID1 = yield insertQuery({'name': 'test1'});
+      var testID2 = yield insertQuery({'name': 'test2'});
+      var testID3 = yield insertQuery({'name': 'test3'});
+      var testIDsArray = [testID1, testID2, testID3];
+
+      var fetchResult = yield fetchQuery(testIDsArray)
+        fetchResult[0].name.should.equal('test1');
+        fetchResult[0]._id.should.equal(testID1);
+        fetchResult[1].name.should.equal('test2');
+        fetchResult[1]._id.should.equal(testID2);
+        fetchResult[2].name.should.equal('test3');
+        fetchResult[2]._id.should.equal(testID3);
         done();
     });
     testFetchData();
