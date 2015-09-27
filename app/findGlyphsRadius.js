345d025dd76ab87687cb717b7395c742f210df43
Richard Artoul <richardartoul@gmail.com>
2015-09-26 18:56:35 -0700

    [Feature] Restructured how return data is formatted

--- app/findGlyphsRadius.js
+++ app/findGlyphsRadius.js
@@ -7,7 +7,18 @@ var findGlyphsRadius = Promise.coroutine(function*(latitude, longitude, radius) 
   var glyphIDs = glyphGeolocationData.map(function(glyph) {
     return glyph.glyphid
   });
-  return yield docStore.fetchData(glyphIDs);
+  var glyphData = yield docStore.fetchData(glyphIDs);
+
+  // Merge postGIS data with Mongo data
+  return glyphData.map(function(data, index) {
+    var glyph = {};
+    glyph.data = data;
+    glyph.id = data._id;
+    delete data._id;
+    glyph.longitude = glyphGeolocationData[index].longitude;
+    glyph.latitude = glyphGeolocationData[index].latitude; 
+    return glyph; 
+  });
 });
 
 module.exports = findGlyphsRadius;
